77976422b5824ab24b35d1084854ae5dfb0a4a98
Tamarinoire <smertniki.j@gmail.com>
2025-12-19 19:00:19 +0700

    app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import plotly.express as px
 st.set_page_config(page_title="Dashboard Efisiensi Ruang Perkuliahan", page_icon="🏫", layout="wide")
 # Styling: light theme (eye-friendly), card container, professional font
 st.markdown(
+    
     """
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap');
