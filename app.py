8a7ba8a806cb72c9338d82a732bec6028dd699c5
Tamarinoire <smertniki.j@gmail.com>
2025-12-19 18:18:37 +0700

    test

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
 st.set_page_config(page_title="Dashboard Efisiensi Ruang Perkuliahan", page_icon="🏫", layout="wide")
-
 # Styling: light theme (eye-friendly), card container, professional font
 st.markdown(
     """
